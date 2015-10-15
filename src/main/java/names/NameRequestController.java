9fb7c6f8e21b78fcf5303bb647367799a00ce815
jpat <jupatterson@guidewire.com>
2015-10-15 16:51:09 +0100

    Retrieve from db using name, year, state

--- src/main/java/names/NameRequestController.java
+++ src/main/java/names/NameRequestController.java
@@ -1,49 +1,51 @@
 package names;
 
-import java.lang.ClassNotFoundException;
 import java.lang.Exception;
+import java.lang.ClassNotFoundException;
+import java.lang.InstantiationException;
+import java.sql.SQLException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.InstantiationException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @RestController
 public class NameRequestController {
 
     @RequestMapping("/name")
-    public NameInfo getNameDataFromDB(@RequestParam(value="name", defaultValue="World") String name){
+    public NameInfo getNameDataFromDB(@RequestParam(value="name", defaultValue="Julia") String nameParam,
+                                      @RequestParam(value="state", defaultValue="PA") String stateParam,
+                                      @RequestParam(value="year", defaultValue="1989") int yearParam){
 
-        String state = "PA";
-        int year = 1998;
-        int frequency = 5;
+        int frequency = 0;
 
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
 
-        String template = "SELECT frequency FROM names WHERE name='%s' AND state='PA' AND year='1989'";
+        String template = "SELECT frequency FROM names WHERE name='%s' AND state='%s' AND year='%s'";
 
         try{
             Class.forName("com.mysql.jdbc.Driver").newInstance();
 
             conn = DriverManager.getConnection("jdbc:mysql://localhost/namesdb", "root", "");
-            System.out.println("connected!!!");
+            System.out.println("Connected to database.");
 
             stmt = conn.createStatement();
 
-            String nameQuery = String.format(template, name);
+            System.out.println("Checking for " + nameParam + " " + stateParam + " " + yearParam);
+            String nameQuery = String.format(template, nameParam,stateParam,yearParam);
             rs = stmt.executeQuery(nameQuery);
 
             while (rs.next()){
                 int freq = rs.getInt("FREQUENCY");
                 System.out.println(freq);
-                frequency = freq;
+                frequency += freq;
             }
         }
         catch(Exception e) {
@@ -51,16 +53,16 @@ public class NameRequestController {
         }
         finally{
 
-//            try {
-//                rs.close();
-//            }catch(Exception e){
-//                System.out.println(e);
-//            }
-//            try {
-//                stmt.close();
-//            }catch(Exception e){
-//                System.out.println(e);
-//            }
+            try {
+                rs.close();
+            }catch(Exception e){
+                System.out.println(e);
+            }
+            try {
+                stmt.close();
+            }catch(Exception e){
+                System.out.println(e);
+            }
             if(conn != null) {
                 try {
                     conn.close();
@@ -71,7 +73,7 @@ public class NameRequestController {
 
         }
 
-        return new NameInfo(state, year, frequency);
+        return new NameInfo(stateParam, yearParam, frequency);
     }
 
 }
