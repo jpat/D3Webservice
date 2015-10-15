9d646f6d8631100ebb67a8d9ec6cedd40f583f28
jpat <jupatterson@guidewire.com>
2015-10-15 16:29:02 +0100

    Retrieve from db using only name

--- src/main/java/names/NameRequestController.java
+++ src/main/java/names/NameRequestController.java
@@ -1,12 +1,16 @@
 package names;
 
+import java.lang.ClassNotFoundException;
 import java.lang.Exception;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.InstantiationException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @RestController
@@ -19,23 +23,55 @@ public class NameRequestController {
         int year = 1998;
         int frequency = 5;
 
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        String template = "SELECT frequency FROM names WHERE name='%s' AND state='PA' AND year='1989'";
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/namesdb", "root", "");
+            System.out.println("connected!!!");
+
+            stmt = conn.createStatement();
+
+            String nameQuery = String.format(template, name);
+            rs = stmt.executeQuery(nameQuery);
+
+            while (rs.next()){
+                int freq = rs.getInt("FREQUENCY");
+                System.out.println(freq);
+                frequency = freq;
+            }
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+        finally{
+
+//            try {
+//                rs.close();
+//            }catch(Exception e){
+//                System.out.println(e);
+//            }
+//            try {
+//                stmt.close();
+//            }catch(Exception e){
+//                System.out.println(e);
+//            }
+            if(conn != null) {
+                try {
+                    conn.close();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+
+        }
+
         return new NameInfo(state, year, frequency);
-
-
-
-//        Connection conn = null;
-//
-//        try{
-//            Class.forName("com.mysql.jdbc.Driver").newInstance();
-//
-//            conn = DriverManager.getConnection("jdbc:mysql://localhost/", "root", "");
-//            System.out.println("connected!!!");
-//
-//        } catch(Exception e){
-//            System.out.println(e);
-//        } finally{
-//            conn.close();
-//        }
     }
 
 }
